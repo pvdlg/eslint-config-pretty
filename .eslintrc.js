8d841dd501135e742035959e337a43134f196910
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-07-13 16:45:09 -0400

    Add promise and ava eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['./es6-config.js', './prettier-config.js'],
+  extends: ['./es6-config.js', './promise-config.js', './ava-config.js', './prettier-config.js'],
   parserOptions: {ecmaVersion: 8, sourceType: 'module'},
   rules: {'no-magic-numbers': 0},
 };
