e5d69ecd0f42e000f3541b33679b9b7b227972a9
Pierre-Denis Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-09-02 01:06:41 -0400

    fix(prettier): disable `function-paren-newline` rule

--- prettier.js
+++ prettier.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: ['prettier'],
   plugins: ['prettier'],
   rules: {
+    // ESLint
+    'function-paren-newline': 0,
+    // Babel plugin
     'babel/object-curly-spacing': 0,
     'babel/semi': 0,
     // Prettier plugin
